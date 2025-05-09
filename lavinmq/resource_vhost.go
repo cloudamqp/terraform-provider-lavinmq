24a80611444c797a2bb6983ea3c7b6b8314319da
Tobias Brodén <tobias@84codes.com>
2025-05-09 12:07:06 +0200

    Remove metadata from Vhost and make the resource simpler
    Commit history still extist to introduce them again.

--- lavinmq/resource_vhost.go
+++ lavinmq/resource_vhost.go
@@ -6,14 +6,12 @@ import (
 
 	"github.com/cloudamqp/terraform-provider-lavinmq/clientlibrary"
 
-	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -26,7 +24,7 @@ var (
 	_ resource.ResourceWithImportState = &vhostResource{}
 )
 
-// NewOrderResource is a helper function to simplify the provider implementation.
+// NewVhostResource is a helper function to simplify the provider implementation.
 func NewVhostResource() resource.Resource {
 	return &vhostResource{}
 }
@@ -37,15 +35,9 @@ type vhostResource struct {
 }
 
 type vhostResourceModel struct {
-	Name                   types.String          `tfsdk:"name"`
-	MaxConnections         types.Int64           `tfsdk:"max_connections"`
-	MaxQueues              types.Int64           `tfsdk:"max_queues"`
-	Dir                    types.String          `tfsdk:"dir"`
-	Tracing                types.Bool            `tfsdk:"tracing"`
-	Messages               types.Int64           `tfsdk:"messages"`
-	MessagesUnacknowledged types.Int64           `tfsdk:"messages_unacknowledged"`
-	MessagesReady          types.Int64           `tfsdk:"messages_ready"`
-	MessagesStats          basetypes.ObjectValue `tfsdk:"message_stats"`
+	Name           types.String `tfsdk:"name"`
+	MaxConnections types.Int64  `tfsdk:"max_connections"`
+	MaxQueues      types.Int64  `tfsdk:"max_queues"`
 }
 
 // Metadata returns the data source type name.
@@ -79,64 +71,6 @@ func (r *vhostResource) Schema(_ context.Context, _ resource.SchemaRequest, resp
 				Default:     nil,
 				PlanModifiers: []planmodifier.Int64{
 					int64planmodifier.UseStateForUnknown(),
-				},
-			},
-			"dir": schema.StringAttribute{
-				Description: "Directory of the vhost.",
-				Computed:    true,
-			},
-			"tracing": schema.BoolAttribute{
-				Description: "Enable or disable tracing for the vhost.",
-				Computed:    true,
-			},
-			"messages": schema.Int64Attribute{
-				Description: "Number of messages in the vhost.",
-				Computed:    true,
-			},
-			"messages_unacknowledged": schema.Int64Attribute{
-				Description: "Number of unacknowledged messages in the vhost.",
-				Computed:    true,
-			},
-			"messages_ready": schema.Int64Attribute{
-				Description: "Number of ready messages in the vhost.",
-				Computed:    true,
-			},
-			"message_stats": schema.SingleNestedAttribute{
-				Description: "Statistics about messages in the vhost.",
-				Computed:    true,
-				Attributes: map[string]schema.Attribute{
-					"ack": schema.Int64Attribute{
-						Description: "Number of acknowledged messages.",
-						Computed:    true,
-					},
-					"confirm": schema.Int64Attribute{
-						Description: "Number of confirmed messages.",
-						Computed:    true,
-					},
-					"deliver": schema.Int64Attribute{
-						Description: "Number of delivered messages.",
-						Computed:    true,
-					},
-					"get": schema.Int64Attribute{
-						Description: "Number of messages retrieved with 'get'.",
-						Computed:    true,
-					},
-					"get_no_ack": schema.Int64Attribute{
-						Description: "Number of messages retrieved with 'get' without acknowledgment.",
-						Computed:    true,
-					},
-					"publish": schema.Int64Attribute{
-						Description: "Number of published messages.",
-						Computed:    true,
-					},
-					"redeliver": schema.Int64Attribute{
-						Description: "Number of redelivered messages.",
-						Computed:    true,
-					},
-					"return_unroutable": schema.Int64Attribute{
-						Description: "Number of messages returned as unroutable.",
-						Computed:    true,
-					},
 				},
 			},
 		},
@@ -183,20 +117,6 @@ func (r *vhostResource) Create(ctx context.Context, req resource.CreateRequest, 
 		}
 	}
 
-	// Read out computed values
-	vhost, err := r.client.Vhosts.Get(ctx, plan.Name.ValueString())
-	if err != nil {
-		resp.Diagnostics.AddError("Failed to read user state", err.Error())
-		return
-	}
-
-	plan.Dir = types.StringValue(vhost.Dir)
-	plan.Messages = types.Int64Value(vhost.Messages)
-	plan.MessagesUnacknowledged = types.Int64Value(vhost.MessagesUnacknowledged)
-	plan.MessagesReady = types.Int64Value(vhost.MessagesReady)
-	plan.Tracing = types.BoolValue(vhost.Tracing)
-	plan.MessagesStats, _ = basetypes.NewObjectValue(r.populateMessageStats(vhost))
-
 	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		tflog.Error(ctx, "created diag failed")
@@ -222,12 +142,6 @@ func (r *vhostResource) Read(ctx context.Context, req resource.ReadRequest, resp
 	}
 
 	state.Name = types.StringValue(vhost.Name)
-	state.Dir = types.StringValue(vhost.Dir)
-	state.Messages = types.Int64Value(vhost.Messages)
-	state.MessagesUnacknowledged = types.Int64Value(vhost.MessagesUnacknowledged)
-	state.MessagesReady = types.Int64Value(vhost.MessagesReady)
-	state.Tracing = types.BoolValue(vhost.Tracing)
-	state.MessagesStats, _ = basetypes.NewObjectValue(r.populateMessageStats(vhost))
 
 	limits, err := r.client.VhostLimits.Get(ctx, state.Name.ValueString())
 	if err != nil {
@@ -280,20 +194,6 @@ func (r *vhostResource) Update(ctx context.Context, req resource.UpdateRequest, 
 		resp.Diagnostics.AddError("Error setting limits", err.Error())
 	}
 
-	// Read out computed values
-	vhost, err := r.client.Vhosts.Get(ctx, plan.Name.ValueString())
-	if err != nil {
-		resp.Diagnostics.AddError("Failed to read user state", err.Error())
-		return
-	}
-
-	plan.Dir = types.StringValue(vhost.Dir)
-	plan.Messages = types.Int64Value(vhost.Messages)
-	plan.MessagesUnacknowledged = types.Int64Value(vhost.MessagesUnacknowledged)
-	plan.MessagesReady = types.Int64Value(vhost.MessagesReady)
-	plan.Tracing = types.BoolValue(vhost.Tracing)
-	plan.MessagesStats, _ = basetypes.NewObjectValue(r.populateMessageStats(vhost))
-
 	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		tflog.Error(ctx, "update diag failed")
@@ -318,28 +218,4 @@ func (r *vhostResource) Delete(ctx context.Context, req resource.DeleteRequest, 
 func (r *vhostResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	// Import resource by name argument
 	resource.ImportStatePassthroughID(ctx, path.Root("name"), req, resp)
-}
-
-func (r *vhostResource) populateMessageStats(vhost *clientlibrary.VhostResponse) (map[string]attr.Type, map[string]attr.Value) {
-	elementTypes := map[string]attr.Type{
-		"ack":               types.Int64Type,
-		"confirm":           types.Int64Type,
-		"deliver":           types.Int64Type,
-		"get":               types.Int64Type,
-		"get_no_ack":        types.Int64Type,
-		"publish":           types.Int64Type,
-		"redeliver":         types.Int64Type,
-		"return_unroutable": types.Int64Type,
-	}
-	elements := map[string]attr.Value{
-		"ack":               types.Int64Value(vhost.MessagesStats.Ack),
-		"confirm":           types.Int64Value(vhost.MessagesStats.Confirm),
-		"deliver":           types.Int64Value(vhost.MessagesStats.Deliver),
-		"get":               types.Int64Value(vhost.MessagesStats.Get),
-		"get_no_ack":        types.Int64Value(vhost.MessagesStats.GetNoAck),
-		"publish":           types.Int64Value(vhost.MessagesStats.Publish),
-		"redeliver":         types.Int64Value(vhost.MessagesStats.Redeliver),
-		"return_unroutable": types.Int64Value(vhost.MessagesStats.ReturnUnroutable),
-	}
-	return elementTypes, elements
 }
