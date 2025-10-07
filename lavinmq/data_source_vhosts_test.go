ba0e1982e8c2556cf207dfc15134e8f5a9f8c9a3
Johan Eckerström <johan@duh.se>
2025-10-07 14:03:59 +0200

    Test Data Source Vhosts (#18)

---
+++ lavinmq/data_source_vhosts_test.go
@@ -0,0 +1,38 @@
+package lavinmq
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccDataSourceVhosts_Basic(t *testing.T) {
+	lavinMQResourceTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+          resource "lavinmq_vhost" "test1" {
+            name = "terraform-lavinmq-test-1"
+          }
+
+          resource "lavinmq_vhost" "test2" {
+            name = "terraform-lavinmq-test-2"
+          }
+
+          data "lavinmq_vhosts" "all" {
+            depends_on = [lavinmq_vhost.test1, lavinmq_vhost.test2]
+          }`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckTypeSetElemNestedAttrs("data.lavinmq_vhosts.all", "vhosts.*", map[string]string{
+						"name": "terraform-lavinmq-test-1",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs("data.lavinmq_vhosts.all", "vhosts.*", map[string]string{
+						"name": "terraform-lavinmq-test-2",
+					}),
+				),
+			},
+		},
+	})
+}
