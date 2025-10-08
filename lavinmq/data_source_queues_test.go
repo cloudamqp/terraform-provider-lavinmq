155c33ce71f9efcb4089c6be531b18fbf82b6575
Tobias Brodén <tobias@84codes.com>
2025-10-08 13:03:54 +0200

    Update tests and VCR recording

--- lavinmq/data_source_queues_test.go
+++ lavinmq/data_source_queues_test.go
@@ -18,16 +18,16 @@ func TestAccDataSourceQueues_Basic(t *testing.T) {
           }
 
           resource "lavinmq_queue" "test1" {
-            name       = "terraform-queue-test-1"
-            vhost      = lavinmq_vhost.test.name
-            durable    = true
+            name        = "terraform-queue-test-1"
+            vhost       = lavinmq_vhost.test.name
+            durable     = true
             auto_delete = false
           }
 
           resource "lavinmq_queue" "test2" {
-            name       = "terraform-queue-test-2"
-            vhost      = lavinmq_vhost.test.name
-            durable    = false
+            name        = "terraform-queue-test-2"
+            vhost       = lavinmq_vhost.test.name
+            durable     = false
             auto_delete = true
           }
 
@@ -37,6 +37,7 @@ func TestAccDataSourceQueues_Basic(t *testing.T) {
           }`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.lavinmq_queues.all", "vhost", "terraform-lavinmq-test"),
+					resource.TestCheckResourceAttrSet("data.lavinmq_queues.all", "queues.#"),
 					resource.TestCheckTypeSetElemNestedAttrs("data.lavinmq_queues.all", "queues.*", map[string]string{
 						"name":        "terraform-queue-test-1",
 						"vhost":       "terraform-lavinmq-test",
@@ -49,6 +50,29 @@ func TestAccDataSourceQueues_Basic(t *testing.T) {
 						"durable":     "false",
 						"auto_delete": "true",
 					}),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceQueues_Empty(t *testing.T) {
+	lavinMQResourceTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+          resource "lavinmq_vhost" "test" {
+            name = "terraform-lavinmq-empty-test"
+          }
+
+          data "lavinmq_queues" "empty" {
+            vhost = lavinmq_vhost.test.name
+          }`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.lavinmq_queues.empty", "vhost", "terraform-lavinmq-empty-test"),
+					resource.TestCheckResourceAttr("data.lavinmq_queues.empty", "queues.#", "0"),
 				),
 			},
 		},
