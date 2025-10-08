01bda474153ed9490d494fb1c9252b962ba6aa57
Tobias Brodén <tobias@84codes.com>
2025-10-08 15:54:25 +0200

    Use default vhost '/' to read data source and check it's a set

--- lavinmq/data_source_queues_test.go
+++ lavinmq/data_source_queues_test.go
@@ -13,43 +13,12 @@ func TestAccDataSourceQueues_Basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: `
-          resource "lavinmq_vhost" "test" {
-            name            = "terraform-lavinmq-test"
-          }
-
-          resource "lavinmq_queue" "test1" {
-            name        = "terraform-queue-test-1"
-            vhost       = lavinmq_vhost.test.name
-            durable     = true
-            auto_delete = false
-          }
-
-          resource "lavinmq_queue" "test2" {
-            name        = "terraform-queue-test-2"
-            vhost       = lavinmq_vhost.test.name
-            durable     = false
-            auto_delete = true
-          }
-
           data "lavinmq_queues" "all" {
-            vhost = lavinmq_vhost.test.name
-            depends_on = [lavinmq_queue.test1, lavinmq_queue.test2]
+            vhost = "/"
           }`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.lavinmq_queues.all", "vhost", "terraform-lavinmq-test"),
+					resource.TestCheckResourceAttr("data.lavinmq_queues.all", "vhost", "/"),
 					resource.TestCheckResourceAttrSet("data.lavinmq_queues.all", "queues.#"),
-					resource.TestCheckTypeSetElemNestedAttrs("data.lavinmq_queues.all", "queues.*", map[string]string{
-						"name":        "terraform-queue-test-1",
-						"vhost":       "terraform-lavinmq-test",
-						"durable":     "true",
-						"auto_delete": "false",
-					}),
-					resource.TestCheckTypeSetElemNestedAttrs("data.lavinmq_queues.all", "queues.*", map[string]string{
-						"name":        "terraform-queue-test-2",
-						"vhost":       "terraform-lavinmq-test",
-						"durable":     "false",
-						"auto_delete": "true",
-					}),
 				),
 			},
 		},
