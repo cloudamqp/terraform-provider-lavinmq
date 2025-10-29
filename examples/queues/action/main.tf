f4f1fc1df55944085a6bb96d773d4310d32ed713
Erica Weistrand <34507514+weistrand@users.noreply.github.com>
2025-10-29 10:28:12 +0100

    Example: publish and purge message

--- examples/queues/action/main.tf
+++ examples/queues/action/main.tf
@@ -6,15 +6,44 @@ resource "lavinmq_vhost" "test" {
   name = "test"
 }
 
-# Create a basic durable queue with default settings
-resource "lavinmq_queue" "basic_example" {
-  name  = "basic-queue"
+resource "lavinmq_exchange" "topic_exchange" {
+  name        = "topic-exchange"
+  vhost       = lavinmq_vhost.test.name
+  type        = "topic"
+  auto_delete = false
+  durable     = true
+}
+
+resource "lavinmq_queue" "example_queue" {
+  name  = "example-queue"
   vhost = lavinmq_vhost.test.name
 }
 
-# Purge the basic queue
+resource "lavinmq_binding" "example_binding" {
+  vhost            = lavinmq_vhost.test.name
+  source           = lavinmq_exchange.topic_exchange.name
+  destination      = lavinmq_queue.example_queue.name
+  destination_type = "queue"
+  routing_key      = "example.*"
+}
+
+resource "lavinmq_publish_message" "example_message" {
+  vhost       = lavinmq_vhost.test.name
+  exchange    = lavinmq_exchange.topic_exchange.name
+  routing_key = "example.rk"
+  payload     = "Hello, LavinMQ!"
+
+  depends_on = [
+    lavinmq_binding.example_binding
+  ]
+}
+
 resource "lavinmq_queue_action" "purge_example" {
-  name   = lavinmq_queue.basic_example.name
+  name   = lavinmq_queue.example_queue.name
   vhost  = lavinmq_vhost.test.name
   action = "purge"
+
+  depends_on = [
+    lavinmq_publish_message.example_message
+  ]
 }
