df4037740185faeb706909c3b200ff68bf628d79
Tobias Brodén <tobias@84codes.com>
2025-10-30 11:00:43 +0100

    VCR-test: Use santizer to make shovel src/dest URI configurable
    For live tests we need to able to change shovel src/dest URI.
    Fetch the URI from .env and use this instead. Set it to, or similar
    `SHOVEL_SRC_DEST_URI="amqp://guest@//"`

--- lavinmq/data_source_shovels_test.go
+++ lavinmq/data_source_shovels_test.go
@@ -1,6 +1,8 @@
 package lavinmq
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -8,34 +10,41 @@ import (
 
 func TestAccDataSourceShovels_Basic(t *testing.T) {
 	t.Parallel()
+
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test1" {
-  name       = "vcr_test_shovel_ds_1"
-  vhost      = "/"
-  src_uri    = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri   = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue  = "source_queue"
-  dest_queue = "dest_queue"
-}
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test1" {
+						name       = "vcr_test_shovel_ds_1"
+						vhost      = "/"
+						src_uri    = "%[1]s"
+						dest_uri   = "%[1]s"
+						src_queue  = "source_queue"
+						dest_queue = "dest_queue"
+					}
 
-resource "lavinmq_shovel" "test2" {
-  name              = "vcr_test_shovel_ds_2"
-  vhost             = "/"
-  src_uri           = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri          = "amqp://guest:guest@localhost:5672/%2f"
-  src_exchange      = "source_exchange"
-  src_exchange_key  = "source.key"
-  dest_queue        = "dest_queue"
-}
+					resource "lavinmq_shovel" "test2" {
+						name              = "vcr_test_shovel_ds_2"
+						vhost             = "/"
+						src_uri           = "%[1]s"
+						dest_uri          = "%[1]s"
+						src_exchange      = "source_exchange"
+						src_exchange_key  = "source.key"
+						dest_queue        = "dest_queue"
+					}
 
-data "lavinmq_shovels" "all" {
-  depends_on = [lavinmq_shovel.test1, lavinmq_shovel.test2]
-}`,
+					data "lavinmq_shovels" "all" {
+						depends_on = [lavinmq_shovel.test1, lavinmq_shovel.test2]
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckTypeSetElemNestedAttrs("data.lavinmq_shovels.all", "shovels.*", map[string]string{
 						"name":       "vcr_test_shovel_ds_1",
--- lavinmq/resource_shovel_test.go
+++ lavinmq/resource_shovel_test.go
@@ -1,6 +1,8 @@
 package lavinmq
 
 import (
+	"fmt"
+	"os"
 	"regexp"
 	"testing"
 
@@ -11,20 +13,26 @@ func TestAccShovel_Import(t *testing.T) {
 	t.Parallel()
 	shovelResourceName := "lavinmq_shovel.test_shovel"
 
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_shovel" {
-  name        = "vcr_test_shovel_import"
-  vhost       = "/"
-  src_uri     = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri    = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue   = "source_queue"
-  dest_queue  = "dest_queue"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_shovel" {
+						name        = "vcr_test_shovel_import"
+						vhost       = "/"
+						src_uri     = "%[1]s"
+						dest_uri    = "%[1]s"
+						src_queue   = "source_queue"
+						dest_queue  = "dest_queue"
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(shovelResourceName, "name", "vcr_test_shovel_import"),
 					resource.TestCheckResourceAttr(shovelResourceName, "vhost", "/"),
@@ -49,22 +57,28 @@ func TestAccShovel_Update(t *testing.T) {
 	t.Parallel()
 	shovelResourceName := "lavinmq_shovel.test_shovel"
 
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_shovel" {
-  name               = "vcr_test_shovel_update"
-  vhost              = "/"
-  src_uri            = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri           = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue          = "source_queue"
-  dest_queue         = "dest_queue"
-  src_prefetch_count = 500
-  ack_mode           = "on-publish"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_shovel" {
+						name               = "vcr_test_shovel_update"
+						vhost              = "/"
+						src_uri            = "%[1]s"
+						dest_uri           = "%[1]s"
+						src_queue          = "source_queue"
+						dest_queue         = "dest_queue"
+						src_prefetch_count = 500
+						ack_mode           = "on-publish"
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(shovelResourceName, "name", "vcr_test_shovel_update"),
 					resource.TestCheckResourceAttr(shovelResourceName, "src_prefetch_count", "500"),
@@ -72,18 +86,18 @@ resource "lavinmq_shovel" "test_shovel" {
 				),
 			},
 			{
-				Config: `
-resource "lavinmq_shovel" "test_shovel" {
-  name               = "vcr_test_shovel_update"
-  vhost              = "/"
-  src_uri            = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri           = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue          = "source_queue"
-  dest_queue         = "dest_queue"
-  src_prefetch_count = 2000
-  ack_mode           = "no-ack"
-  reconnect_delay    = 10
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_shovel" {
+						name               = "vcr_test_shovel_update"
+						vhost              = "/"
+						src_uri            = "%[1]s"
+						dest_uri           = "%[1]s"
+						src_queue          = "source_queue"
+						dest_queue         = "dest_queue"
+						src_prefetch_count = 2000
+						ack_mode           = "no-ack"
+						reconnect_delay    = 10
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(shovelResourceName, "name", "vcr_test_shovel_update"),
 					resource.TestCheckResourceAttr(shovelResourceName, "src_prefetch_count", "2000"),
@@ -99,20 +113,26 @@ func TestAccShovel_QueueToQueue(t *testing.T) {
 	t.Parallel()
 	shovelResourceName := "lavinmq_shovel.test_q2q"
 
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_q2q" {
-  name       = "vcr_test_q2q"
-  vhost      = "/"
-  src_uri    = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri   = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue  = "source_queue"
-  dest_queue = "dest_queue"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_q2q" {
+						name       = "vcr_test_q2q"
+						vhost      = "/"
+						src_uri    = "%[1]s"
+						dest_uri   = "%[1]s"
+						src_queue  = "source_queue"
+						dest_queue = "dest_queue"
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(shovelResourceName, "name", "vcr_test_q2q"),
 					resource.TestCheckResourceAttr(shovelResourceName, "src_queue", "source_queue"),
@@ -129,21 +149,27 @@ func TestAccShovel_QueueToExchange(t *testing.T) {
 	t.Parallel()
 	shovelResourceName := "lavinmq_shovel.test_q2e"
 
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_q2e" {
-  name             = "vcr_test_q2e"
-  vhost            = "/"
-  src_uri          = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri         = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue        = "source_queue"
-  dest_exchange    = "dest_exchange"
-  dest_exchange_key = "routing.key"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_q2e" {
+						name             = "vcr_test_q2e"
+						vhost            = "/"
+						src_uri          = "%[1]s"
+						dest_uri         = "%[1]s"
+						src_queue        = "source_queue"
+						dest_exchange    = "dest_exchange"
+						dest_exchange_key = "routing.key"
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(shovelResourceName, "name", "vcr_test_q2e"),
 					resource.TestCheckResourceAttr(shovelResourceName, "src_queue", "source_queue"),
@@ -159,21 +185,27 @@ func TestAccShovel_ExchangeToQueue(t *testing.T) {
 	t.Parallel()
 	shovelResourceName := "lavinmq_shovel.test_e2q"
 
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_e2q" {
-  name              = "vcr_test_e2q"
-  vhost             = "/"
-  src_uri           = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri          = "amqp://guest:guest@localhost:5672/%2f"
-  src_exchange      = "source_exchange"
-  src_exchange_key  = "source.key"
-  dest_queue        = "dest_queue"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_e2q" {
+						name              = "vcr_test_e2q"
+						vhost             = "/"
+						src_uri           = "%[1]s"
+						dest_uri          = "%[1]s"
+						src_exchange      = "source_exchange"
+						src_exchange_key  = "source.key"
+						dest_queue        = "dest_queue"
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(shovelResourceName, "name", "vcr_test_e2q"),
 					resource.TestCheckResourceAttr(shovelResourceName, "src_exchange", "source_exchange"),
@@ -189,22 +221,28 @@ func TestAccShovel_ExchangeToExchange(t *testing.T) {
 	t.Parallel()
 	shovelResourceName := "lavinmq_shovel.test_e2e"
 
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_e2e" {
-  name              = "vcr_test_e2e"
-  vhost             = "/"
-  src_uri           = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri          = "amqp://guest:guest@localhost:5672/%2f"
-  src_exchange      = "source_exchange"
-  src_exchange_key  = "source.key"
-  dest_exchange     = "dest_exchange"
-  dest_exchange_key = "dest.key"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_e2e" {
+						name              = "vcr_test_e2e"
+						vhost             = "/"
+						src_uri           = "%[1]s"
+						dest_uri          = "%[1]s"
+						src_exchange      = "source_exchange"
+						src_exchange_key  = "source.key"
+						dest_exchange     = "dest_exchange"
+						dest_exchange_key = "dest.key"
+					}`, testSrcDestURI),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(shovelResourceName, "name", "vcr_test_e2e"),
 					resource.TestCheckResourceAttr(shovelResourceName, "src_exchange", "source_exchange"),
@@ -219,21 +257,28 @@ resource "lavinmq_shovel" "test_e2e" {
 
 func TestAccShovel_InvalidBothSources(t *testing.T) {
 	t.Parallel()
+
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_invalid" {
-  name         = "vcr_test_invalid"
-  vhost        = "/"
-  src_uri      = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri     = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue    = "source_queue"
-  src_exchange = "source_exchange"
-  dest_queue   = "dest_queue"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_invalid" {
+						name         = "vcr_test_invalid"
+						vhost        = "/"
+						src_uri      = "%[1]s"
+						dest_uri     = "%[1]s"
+						src_queue    = "source_queue"
+						src_exchange = "source_exchange"
+						dest_queue   = "dest_queue"
+					}`, testSrcDestURI),
 				ExpectError: regexp.MustCompile(`Cannot specify both src_queue and src_exchange`),
 			},
 		},
@@ -242,21 +287,28 @@ resource "lavinmq_shovel" "test_invalid" {
 
 func TestAccShovel_InvalidBothDestinations(t *testing.T) {
 	t.Parallel()
+
+	// Set sanitized value for playback and use real value for recording
+	testSrcDestURI := "SHOVEL_SRC_DEST_URI"
+	if os.Getenv("LAVINMQ_RECORD") != "" {
+		testSrcDestURI = os.Getenv("SHOVEL_SRC_DEST_URI")
+	}
+
 	lavinMQResourceTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "lavinmq_shovel" "test_invalid" {
-  name          = "vcr_test_invalid"
-  vhost         = "/"
-  src_uri       = "amqp://guest:guest@localhost:5672/%2f"
-  dest_uri      = "amqp://guest:guest@localhost:5672/%2f"
-  src_queue     = "source_queue"
-  dest_queue    = "dest_queue"
-  dest_exchange = "dest_exchange"
-}`,
+				Config: fmt.Sprintf(`
+					resource "lavinmq_shovel" "test_invalid" {
+						name          = "vcr_test_invalid"
+						vhost         = "/"
+						src_uri       = "%[1]s"
+						dest_uri      = "%[1]s"
+						src_queue     = "source_queue"
+						dest_queue    = "dest_queue"
+						dest_exchange = "dest_exchange"
+					}`, testSrcDestURI),
 				ExpectError: regexp.MustCompile(`Cannot specify both dest_queue and dest_exchange`),
 			},
 		},
