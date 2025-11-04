be44f37b1840ae07432dfa6691700aa435a990a7
Johan Eckerström <johan.e@84codes.com>
2025-11-04 13:53:37 +0100

    Remove unused client field from Services struct
    The client field was not used, all services use their own client reference.

--- clientlibrary/services.go
+++ clientlibrary/services.go
@@ -1,9 +1,6 @@
 package clientlibrary
 
 type Services struct {
-	// TODO: not needed
-	client *Client
-
 	Users       *UsersService
 	VhostLimits *VhostLimitsService
 	Vhosts      *VhostsService
@@ -18,7 +15,6 @@ type Services struct {
 
 func NewServices(client *Client) *Services {
 	return &Services{
-		client:      client,
 		Users:       (*UsersService)(&service{client: client}),
 		VhostLimits: (*VhostLimitsService)(&service{client: client}),
 		Vhosts:      (*VhostsService)(&service{client: client}),
