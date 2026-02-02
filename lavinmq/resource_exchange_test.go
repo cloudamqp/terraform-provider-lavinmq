a118606eb62230fc5152eba00ec1c00bfc0ac460
Tobias Brodén <47317658+tbroden84@users.noreply.github.com>
2026-02-02 14:28:47 +0100

    Fix: Handle 404 errors for missing parent resources to prevent nil pointer crashes (#91)
    * ClientLibrary: Handle non existing parent resource during create/update.
    * VCR-test: Update coverage with missing vhost, name test cases
    
    Closes: #87, #89

--- lavinmq/resource_exchange_test.go
+++ lavinmq/resource_exchange_test.go
@@ -2,6 +2,7 @@ package lavinmq
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -179,27 +180,6 @@ func TestAccExchange_BooleanAttributes(t *testing.T) {
 	})
 }
 
-func TestAccExchange_Drift(t *testing.T) {
-	t.Parallel()
-	lavinMQResourceTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: `
-          resource "lavinmq_exchange" "vcr_test" {
-            name        = "vcr_test_drift"
-            vhost       = "/"
-            type        = "direct"
-            auto_delete = false
-            durable     = false
-          }`,
-				ExpectNonEmptyPlan: true,
-			},
-		},
-	})
-}
-
 func TestAccExchange_WithArguments(t *testing.T) {
 	t.Parallel()
 	exchangeResourceName := "lavinmq_exchange.test_exchange"
@@ -236,6 +216,28 @@ func TestAccExchange_WithArguments(t *testing.T) {
 				ImportStateId:                        "/@vcr_test_exchange_with_arguments",
 				ImportState:                          true,
 				ImportStateVerify:                    true,
+			},
+		},
+	})
+}
+
+func TestAccExchange_MissingVhost(t *testing.T) {
+	t.Parallel()
+
+	lavinMQResourceTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+          resource "lavinmq_exchange" "vcr_test" {
+            name        = "vcr_test_exchange_missing_vhost"
+            vhost       = "nonexistent_vhost"
+            type        = "direct"
+            auto_delete = false
+            durable     = false
+          }`,
+				ExpectError: regexp.MustCompile(`status code: 404|parent resource may not exist|vhost.*not found`),
 			},
 		},
 	})
